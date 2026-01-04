aa0a3ca9877dcf1706002a9e79b153e5f7181c48
mushan <xiongshunbin@outlook.com>
2026-01-04 21:52:30 +0800

    add: 1 bit digital tube

---
+++ Stage02/09_Digital_Tube/User/bsp_digital_tube.h
@@ -0,0 +1,10 @@
+#ifndef __BSP_DIGITAL_TUBE_H__
+#define __BSP_DIGITAL_TUBE_H__
+
+#include <stm32f10x.h>
+
+extern unsigned int num[];
+
+void Digital_Tube_Configuration(void);
+
+#endif	// __BSP_DIGITAL_TUBE_H__
---
+++ Stage02/09_Digital_Tube/User/main.c
@@ -0,0 +1,32 @@
+#include "LED_Key.h"
+#include "bsp_exti.h"
+#include "bsp_SysTick.h"
+#include "bsp_iwdg.h"
+#include "bsp_wwdg.h"
+#include "bsp_uart.h"
+#include "bsp_dma.h"
+#include "bsp_adc.h"
+#include "bsp_tim.h"
+#include "bsp_rtc.h"
+#include "music.h"
+#include "bsp_hcsr04.h"
+#include "bsp_spi_max6675.h"
+#include "bsp_digital_tube.h"
+
+int main(void)
+{
+	int i;
+	
+	SysTick_Configuration();
+	
+	Digital_Tube_Configuration();
+	
+	while (1)
+	{
+		for (i = 0; i < 18; ++i)
+		{
+			GPIOA->ODR = num[i];
+			Delay_us(500000);
+		}
+	}
+}
